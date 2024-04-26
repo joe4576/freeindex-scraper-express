388727fff8e8b11d30b999ca9e4be7b3ebf2dd53
Joe Brown <joebrown.4576@gmail.com>
2024-04-26 20:37:40 +0100

    Error wrapper

--- dist/index.js
+++ dist/index.js
@@ -12,7 +12,16 @@ app.use((0, cors_1.default)());
 app.get("/", (req, res) => {
     res.send("Hello World! With typescript without manual build");
 });
-app.get("/freeindex/:profile", async (req, res) => {
+const wrapWithErrorHandling = async (fn, errorHandler) => {
+    try {
+        return await fn();
+    }
+    catch (error) {
+        console.error(error);
+        return errorHandler(error);
+    }
+};
+app.get("/freeindex/:profile", async (req, res) => wrapWithErrorHandling(async () => {
     const profile = req.params.profile;
     const url = `https://www.freeindex.co.uk/${profile}`;
     const response = await fetch(url);
@@ -25,10 +34,12 @@ app.get("/freeindex/:profile", async (req, res) => {
     }
     const numberOfReviews = reviewsText.split(" ")[0]?.replaceAll(",", "") ?? null;
     if (numberOfReviews === null) {
-        return res.status(500).json({ error: "Could not find number of reviews" });
+        return res
+            .status(500)
+            .json({ error: "Could not find number of reviews" });
     }
     res.status(200).json({ numberOfReviews });
-});
+}, (error) => res.status(500).json({ error })));
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
--- index.ts
+++ index.ts
@@ -11,31 +11,50 @@ app.get("/", (req, res) => {
   res.send("Hello World! With typescript without manual build");
 });
 
-app.get("/freeindex/:profile", async (req, res) => {
-  const profile = req.params.profile;
-  const url = `https://www.freeindex.co.uk/${profile}`;
-
-  const response = await fetch(url);
-  const data = await response.text();
-
-  const $ = load(data);
-  const reviewsText = $("h2#reviewsTop").text();
-
-  console.log({ reviewsText });
-
-  if (!reviewsText) {
-    return res.status(404);
+const wrapWithErrorHandling = async <T = unknown>(
+  fn: (...args: any[]) => Promise<T>,
+  errorHandler: (error: unknown) => any
+) => {
+  try {
+    return await fn();
+  } catch (error: unknown) {
+    console.error(error);
+    return errorHandler(error);
   }
+};
 
-  const numberOfReviews =
-    reviewsText.split(" ")[0]?.replaceAll(",", "") ?? null;
+app.get("/freeindex/:profile", async (req, res) =>
+  wrapWithErrorHandling(
+    async () => {
+      const profile = req.params.profile;
+      const url = `https://www.freeindex.co.uk/${profile}`;
 
-  if (numberOfReviews === null) {
-    return res.status(500).json({ error: "Could not find number of reviews" });
-  }
+      const response = await fetch(url);
+      const data = await response.text();
 
-  res.status(200).json({ numberOfReviews });
-});
+      const $ = load(data);
+      const reviewsText = $("h2#reviewsTop").text();
+
+      console.log({ reviewsText });
+
+      if (!reviewsText) {
+        return res.status(404);
+      }
+
+      const numberOfReviews =
+        reviewsText.split(" ")[0]?.replaceAll(",", "") ?? null;
+
+      if (numberOfReviews === null) {
+        return res
+          .status(500)
+          .json({ error: "Could not find number of reviews" });
+      }
+
+      res.status(200).json({ numberOfReviews });
+    },
+    (error) => res.status(500).json({ error })
+  )
+);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);

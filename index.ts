d765d53f85c81743642b284b159b8f6d3d07e725
Joe Brown <joebrown.4576@gmail.com>
2024-04-26 20:15:31 +0100

    Small change

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ const port = 3000;
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Hello World! With typescript");
+  res.send("Hello World! With typescript without manual build");
 });
 
 app.listen(port, () => {
